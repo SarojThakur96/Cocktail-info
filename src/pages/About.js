203df7a88e24d729a82d3335004e78348092dd65
Saroj Thakur <sarojthakur247@gmail.com>
2021-03-12 09:49:04 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        As, we know everday some new cocktail roll out in a market , so before going to drink. we should have knowledage about some cocktails, so thats why this website created to give some information about cocktails.
+      </p>
+    </section>
+  )
+}
+
+export default About
